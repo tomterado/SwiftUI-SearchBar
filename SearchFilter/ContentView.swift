a082c0d8999dfe79f174b7728d67ee0e8f53481b
Tom Terado <tom.terado@gmail.com>
2020-09-21 12:22:39 -0700

    Initial Commit

---
+++ SearchFilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchFilter
+//
+//  Created by Tom Terado on 21/9/20.
+//  Copyright © 2020 Tom Terado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
